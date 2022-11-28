c7bf7cf5628670aa8f5e4fa9679465adeb300f1e
kefirzmolokom <tumyrb05@gmail.com>
2022-11-28 21:20:45 +0200

    adding my lab5 code

---
+++ lab5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <lab5.h>
+
+int main()
+{
+    TimeConverter timeconverter;
+    int seconds;
+    std::string time;
+    int UserInput;
+
+    std::cout << "from hh:mm:ss to seconds or vice versa?"
+              << std::endl
+              << "print 1 for first option and 0 for second: ";
+    std::cin >> UserInput;
+
+    if (UserInput == 1)
+    {
+        std::cout << "now input hh:mm:ss -> ";
+        std::cin >> time;
+        timeconverter.outputConvertedTime(time);
+    }
+    else
+    {
+        std::cout << "now input seconds -> ";
+        std::cin >> seconds;
+        timeconverter.outputConvertedTime(seconds);
+    }
+    return 0;
+}
